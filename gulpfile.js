355f64fc4b238f58847660c65b97e2c9b5456f46
Kirill Novik <jimmynovik@gmail.com>
2015-12-31 03:29:16 +0000

    Added UX and Best Practices and Mobile Optimizations

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var LessPluginCleanCSS = require('less-plugin-clean-css'),
+    cleancss = new LessPluginCleanCSS({ advanced: true });
 
 gulp.task('less', function() {
   console.log("Processing Less Files...");
   return gulp.src('./src/less/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'node_modules', 'less', 'dist') ]
+    }))
+    .pipe(less({
+      plugins: [cleancss]
     }))
     .pipe(gulp.dest('./src/css'));
 });
